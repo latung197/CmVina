d1b3d066ff4ac68f0c9f86f468f4674f4ae47739
DESKTOP-9F5APHK\Admin <levantung0000@gmail.com>
2023-05-21 23:03:26 +0700

    tung

--- CmVina.Model/Abstract/AudiTable.cs
+++ CmVina.Model/Abstract/AudiTable.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace CmVina.Model.Abstract
 {
     public abstract class AudiTable : IAudiTable
     {
+        [MaxLength(256)]
         public string UserID0 { get ; set ; }
         public DateTime? Date0 { get ; set ; }
         public DateTime? Time0 { get ; set ; }
+        [MaxLength(256)]
         public string UserID2 { get ; set ; }
         public DateTime? Date2 { get ; set ; }
         public DateTime? Time2 { get ; set ; }
+        [MaxLength(256)]
         public string MetaKeyword { get ; set ; }
+        [MaxLength(256)]
         public string MetaDescription { get ; set ; }
         public bool Status { get ; set ; }
     }
--- CmVina.Model/Models/Error.cs
+++ CmVina.Model/Models/Error.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CmVina.Model.Abstract;
 
 namespace CmVina.Model.Models
 {
-    public class Error
+    public class Error:AudiTable
     {
+        [MaxLength(25)]
+        [Key]
+        public string errorID { get; set; }
+        [Required]
+        public string errorName { get; set; }
+        public string procesID { get; set; }
+        
     }
 }
